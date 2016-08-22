9f73a0f07b35bc26ec378311020e5ba73cdc57b0
Andrii Vasyliev <sol@hiqdev.com>
2016-08-22 06:07:20 +0000

    version bump to 0.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# hiqdev/hisite-core
+
+## [0.0.1] - 2016-08-22
+
+- Added basics
+
+## [Development started] - 2016-08-09
--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Hisite core package
+ *
+ * @link      https://github.com/hiqdev/hisite-core
+ * @package   hisite-core
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
 error_reporting(E_ALL & ~E_NOTICE);
 
 $bootstrap = __DIR__ . '/../src/_bootstrap.php';
