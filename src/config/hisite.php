388eec1c5e6abeafd97636e9a9c416e86fdc7ea6
Andrii Vasyliev <sol@hiqdev.com>
2017-01-13 17:07:27 +0000

    removed urlManager default rules, looks like they are not needed

--- src/config/hisite.php
+++ src/config/hisite.php
@@ -61,11 +61,6 @@ return array_filter([
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'enableStrictParsing' => false,
-            'rules' => [
-                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
-                '<_c:[\w\-]+>' => '<_c>/index',
-                '<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_c>/<_a>',
-            ],
         ],
         'themeManager' => [
             'pathMap' => [
