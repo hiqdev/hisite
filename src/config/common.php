bbc242d59486608320fb3869033a721773224dc1
Andrii Vasyliev <sol@hiqdev.com>
2018-07-08 14:12:39 +0300

    redone config for yii 3.0

--- src/config/common.php
+++ src/config/common.php
@@ -9,52 +9,51 @@
  */
 
 return array_filter([
-    'id' => $params['app.id'],
-    'name' => $params['app.name'],
-    'language' => $params['app.language'],
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm' => '@vendor/npm-asset',
-        '@vendor/bower' => '@vendor/bower-asset',
-        '@vendor/npm' => '@vendor/npm-asset',
+    \yii\base\Application::class => function () {
+        return Yii::$app;
+    },
+    \yii\mail\MailerInterface::class => function () {
+        return Yii::$app->get('mailer');
+    },
+
+    'application' => [
+        'id' => $params['app.id'],
+        'name' => $params['app.name'],
+        'language' => $params['app.language'],
+        'aliases' => [
+            '@bower' => '@vendor/bower-asset',
+            '@npm' => '@vendor/npm-asset',
+            '@vendor/bower' => '@vendor/bower-asset',
+            '@vendor/npm' => '@vendor/npm-asset',
+        ],
+        'basePath' => dirname(__DIR__),
+        'viewPath' => '@hisite/views',
+        'vendorPath' => '@root/vendor',
+        'runtimePath' => '@root/runtime',
+        'controllerNamespace' => 'hisite\controllers',
+        'bootstrap' => array_filter([
+            'log' => 'log',
+        ]),
     ],
-    'basePath' => dirname(__DIR__),
-    'viewPath' => '@hisite/views',
-    'vendorPath' => '@root/vendor',
-    'runtimePath' => '@root/runtime',
-    'controllerNamespace' => 'hisite\controllers',
-    'bootstrap' => array_filter([
-        'log' => 'log',
-    ]),
-    'components' => [
-        'mailer' => [
-            'class' => \yii\swiftmailer\Mailer::class,
-            'viewPath' => '@hisite/views/mail',
-            'htmlLayout' => '@hisite/views/layouts/mail-html',
-            'textLayout' => '@hisite/views/layouts/mail-text',
-            'useFileTransport' => $params['mailer.enabled'] ? false : true,
-        ],
-        'cache' => [
-            'class' => \yii\caching\FileCache::class,
-        ],
-        'i18n' => [
-            'class' => \yii\i18n\I18N::class,
-            'translations' => [
-                'hisite' => [
-                    'class' => \yii\i18n\PhpMessageSource::class,
-                    'basePath' => '@hisite/messages',
-                ],
+
+    'mailer' => [
+        'class' => \yii\swiftmailer\Mailer::class,
+        'viewPath' => '@hisite/views/mail',
+        'htmlLayout' => '@hisite/views/layouts/mail-html',
+        'textLayout' => '@hisite/views/layouts/mail-text',
+        'useFileTransport' => $params['mailer.enabled'] ? false : true,
+    ],
+    'cache' => [
+        'class' => \yii\caching\FileCache::class,
+    ],
+    'i18n' => [
+        'class' => \yii\i18n\I18N::class,
+        'translations' => [
+            'hisite' => [
+                'class' => \yii\i18n\PhpMessageSource::class,
+                'basePath' => '@hisite/messages',
             ],
         ],
     ],
-    'container' => [
-        'singletons' => [
-            \yii\base\Application::class => function () {
-                return Yii::$app;
-            },
-            \yii\mail\MailerInterface::class => function () {
-                return Yii::$app->get('mailer');
-            },
-        ],
-    ],
+
 ]);
--- src/config/web.php
+++ src/config/web.php
@@ -8,49 +8,52 @@
  * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
  */
 
+use yii\di\Reference;
+
 return array_filter([
-    'bootstrap' => array_filter([
-        'debug' => empty($params['debug.enabled']) ? null : 'debug',
-    ]),
-    'components' => [
-        'request' => [
-            'enableCsrfCookie' => true, /// XXX TO BE DISABLED
-            'cookieValidationKey' => $params['cookieValidationKey'],
-        ],
-        'user' => [
-            'identityClass' => \hisite\models\User::class,
-            'enableAutoLogin' => true,
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'urlManager' => [
-            'class' => \yii\web\UrlManager::class,
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'enableStrictParsing' => false,
-        ],
-        'themeManager' => [
-            'pathMap' => [
-                '$themedViewPaths' => ['@hisite/views'],
-            ],
+    \yii\web\Application::class => Reference::to('application'),
+    'application' => [
+        'bootstrap' => array_filter([
+            'debug' => $params['debug.enabled'] ?? null,
+        ]),
+        'modules' => array_filter([
+            'debug' => empty($params['debug.enabled']) ? null : array_filter([
+                'class' => \yii\debug\Module::class,
+                'allowedIPs' => $params['debug.allowedIps'],
+                'historySize' => $params['debug.historySize'],
+            ]),
+        ]),
+    ],
+
+    /// components
+    'request' => [
+        'enableCsrfCookie' => true, /// XXX TO BE DISABLED
+        'cookieValidationKey' => $params['cookieValidationKey'],
+    ],
+    'user' => [
+        'identityClass' => \hisite\models\User::class,
+        'enableAutoLogin' => true,
+    ],
+    'errorHandler' => [
+        'errorAction' => 'site/error',
+    ],
+    'urlManager' => [
+        'class' => \yii\web\UrlManager::class,
+        'enablePrettyUrl' => true,
+        'showScriptName' => false,
+        'enableStrictParsing' => false,
+    ],
+    'themeManager' => [
+        'pathMap' => [
+            '$themedViewPaths' => ['@hisite/views'],
         ],
     ],
-    'modules' => array_filter([
-        'debug' => empty($params['debug.enabled']) ? null : array_filter([
-            'class' => \yii\debug\Module::class,
-            'allowedIPs' => $params['debug.allowedIps'],
-            'historySize' => $params['debug.historySize'],
-        ]),
-    ]),
-    'container' => [
-        'singletons' => [
-            \hiqdev\thememanager\menus\AbstractMainMenu::class => [
-                'class' => \hisite\menus\MainMenu::class,
-            ],
-            \hiqdev\thememanager\menus\AbstractFooterMenu::class => [
-                'class' => \hisite\menus\FooterMenu::class,
-            ],
-        ],
+
+    /// menus
+    \hiqdev\thememanager\menus\AbstractMainMenu::class => [
+        'class' => \hisite\menus\MainMenu::class,
+    ],
+    \hiqdev\thememanager\menus\AbstractFooterMenu::class => [
+        'class' => \hisite\menus\FooterMenu::class,
     ],
 ]);
